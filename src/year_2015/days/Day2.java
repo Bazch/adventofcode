738f32909209e4863494f4ec99693e211c430cfb
Bas van den Boomgaard <bvdboomg@betterbe.com>
2021-02-20 16:08:35 +0100

    Solution to Day-2:1. Halfway to Day-2:2

--- src/year_2015/days/Day2.java
+++ src/year_2015/days/Day2.java
@@ -1,5 +1,11 @@
 package year_2015.days;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Day2 implements Day {
 
 	private String input = readFileToString(FILE_PATH);
@@ -11,7 +17,7 @@ public class Day2 implements Day {
 		return array;
 	}
 
-	public int calculate() {
+	public int calculateWrapping() {
 		int l = 0;
 		int w = 0;
 		int h = 0;
@@ -23,20 +29,46 @@ public class Day2 implements Day {
 			l = Integer.parseInt(array2[0]);
 			w = Integer.parseInt(array2[1]);
 			h = Integer.parseInt(array2[2]);
-			smallest = Math.min(l, w);
-			smallest = Math.min(smallest, h);
+			List<Integer> newArray = new ArrayList<Integer>(Arrays.asList(l*w,w*h,h*l));
+			Collections.sort(newArray);
+			smallest = newArray.get(0);
 
 			System.out.println("Total: " + total + " + (2 * " + l + " * " + w + " ) + (2 * " + w + " * " + h + ") + (2 * " + h + " * " + l + ") + " + smallest);
-			total += ((2 * l * w) + (2 * w * h) + (2 * h * l) + smallest);
-			System.out.println(total);
+			total += (((2 * l * w) + (2 * w * h) + (2 * h * l)) + smallest);
+			System.out.println(i);
 		}
 
 		return total;
 	}
 
+	public int calculateRibbon() {
+		int l = 0;
+		int w = 0;
+		int h = 0;
+		int smallest = 0;
+		int total = 0;
+		String[] array = input.split("\n");
+		for (int i = 0; i < array.length; i++) {
+			String[] array2 = array[i].split("x");
+			l = Integer.parseInt(array2[0]);
+			w = Integer.parseInt(array2[1]);
+			h = Integer.parseInt(array2[2]);
+			List<Integer> newArray = new ArrayList<Integer>(Arrays.asList(l*w,w*h,h*l));
+			Collections.sort(newArray);
+			smallest = newArray.get(0);
+
+			System.out.println("Total: " + total + " + (2 * " + l + " * " + w + " ) + (2 * " + w + " * " + h + ") + (2 * " + h + " * " + l + ") + " + smallest);
+			total += (((2 * l * w) + (2 * w * h) + (2 * h * l)) + smallest);
+			System.out.println(i);
+		}
+
+		return total;
+	}
+
+	
 	@Override
 	public void execute() {
 
-		System.out.println(calculate());
+		System.out.println(calculateWrapping());
 	}
 }
